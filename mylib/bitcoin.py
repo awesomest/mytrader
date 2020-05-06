481daa822d10a22a8bf9b73f81a1840407a17107
Shuhei Katori <ihad.19.n@gmail.com>
2020-05-07 03:31:56 +0900

    Create the model from 80% of dataset

--- mylib/bitcoin.py
+++ mylib/bitcoin.py
@@ -10,28 +10,7 @@ class BitcoinRegression:
         self.data_test = None
         self.label_train = None
         self.label_test = None
-
-    def set_dataset(self, csv):
-        self.data = csv.sort_values(["unixtime"])
-
-    def train(self):
-        self.model = LinearRegression()
-        self.model.fit(self.data_train, self.label_train)
-
-    def calc_avg_pred(self):
-        sum_score = 0
-        times = 100
-        for i in range(times):
-            self.set_train_test_dataset(0.2)
-            self.train()
-            ac_score = self.model.score(self.data_test, self.label_test)
-            print(f"score[{i:02}]: {ac_score}")
-            sum_score += ac_score
-
-        return sum_score / times
-
-    def set_train_test_dataset(self, test_ratio):
-        train_columns = [
+        self.train_columns = [
             "volume",
             "high_ratio",
             "low_ratio",
@@ -53,7 +32,27 @@ class BitcoinRegression:
             "close_ratio10080",
         ]
 
-        _x = self.data[train_columns]
+    def set_dataset(self, csv):
+        self.data = csv.sort_values(["unixtime"])
+
+    def train(self):
+        self.model = LinearRegression()
+        self.model.fit(self.data_train, self.label_train)
+
+    def calc_avg_score(self):
+        sum_score = 0
+        times = 100
+        for i in range(times):
+            self.set_train_test_dataset(0.2)
+            self.train()
+            score = self.model.score(self.data_test, self.label_test)
+            print(f"score[{i:03}]: {score}")
+            sum_score += score
+
+        return sum_score / times
+
+    def set_train_test_dataset(self, test_ratio):
+        _x = self.data[self.train_columns]
         _y = self.data["result"]
         (
             self.data_train,
