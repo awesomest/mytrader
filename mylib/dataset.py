c3f5604e5812bbc6eac047f005edb17f445ad110
Shuhei Katori <ihad.19.n@gmail.com>
2020-05-11 11:52:40 +0900

    Save csv after every adding column

--- mylib/dataset.py
+++ mylib/dataset.py
@@ -20,7 +20,8 @@ class BitcoinDataset:
     BitcoinDataset
     """
 
-    def __init__(self):
+    def __init__(self, version):
+        self.version = version
         self.data = None
 
     def set_dataset(self, csv):
@@ -38,23 +39,17 @@ class BitcoinDataset:
         self.add_columns_time()
         self.convert_hlc_to_ratio()
         self.remove_missing_rows()
+        self.data.to_csv("datasets/" + str(self.version) + ".csv", index=False)
         logger.info("end: {:s}".format(inspect.currentframe().f_code.co_name))
-
-    def set_dataset_for_test(self, csv):
-        """
-        TODO: テスト用なので、不要になったら削除
-        テスト用データセット作成
-        """
-        logger.info("start: {:s}".format(inspect.currentframe().f_code.co_name))
-        self.data = csv
-        self.rename_result_column()
-        self.add_column_extreme_60_later()
 
     def add_column_extreme_60_later(self):
         """
         現在から60分後までの最大値or最小値を追加
         """
         logger.info("start: {:s}".format(inspect.currentframe().f_code.co_name))
+        if "extreme60" in self.data.columns:
+            return
+
         for index, row in self.data.iterrows():
             if index % 10000 == 0:
                 logger.info("  index: {:.2%}".format(index / len(self.data)))
@@ -75,24 +70,22 @@ class BitcoinDataset:
 
             self.data.at[index, "extreme60"] = extreme60
 
-    def rename_result_column(self):
-        """
-        TODO: 一時的なものなので、不要になったら削除
-        カラム名を変更
-        """
-        logger.info("start: {:s}".format(inspect.currentframe().f_code.co_name))
-        self.data = self.data.rename(columns={"result": "trend60"})
+        self.data.to_csv("datasets/" + str(self.version) + ".csv", index=False)
 
     def add_columns_time(self):
         """
         現在日時に関する列を追加
         """
         logger.info("start: {:s}".format(inspect.currentframe().f_code.co_name))
+        if "second" in self.data.columns:
+            return
+
         timestamp = pd.Series([dt.fromtimestamp(i) for i in self.data["unixtime"]])
         self.data["day"] = timestamp.dt.day
         self.data["weekday"] = timestamp.dt.dayofweek
         self.data["second"] = (timestamp.dt.hour * 60 + timestamp.dt.minute) * 60
         self.data.drop(columns=["unixtime"], inplace=True)
+        self.data.to_csv("datasets/" + str(self.version) + ".csv", index=False)
 
     def remove_missing_rows(self):
         """
@@ -106,6 +99,9 @@ class BitcoinDataset:
         60分前から現在までのトレンドを追加
         """
         logger.info("start: {:s}".format(inspect.currentframe().f_code.co_name))
+        if "trend" in self.data.columns:
+            return
+
         for index, _ in self.data.iterrows():
             if index % 10000 == 0:
                 logger.info("  index: {:.2%}".format(index / len(self.data)))
@@ -119,12 +115,18 @@ class BitcoinDataset:
             trend_line = linregress(x=_x, y=data_in_hours["close"])
             self.data.at[index, "trend"] = trend_line[0] / data_in_hours["close"].iat[0]
 
+        self.data.to_csv("datasets/" + str(self.version) + ".csv", index=False)
+
     def add_column_trend_60_later(self):
         """
         現在から60分後までのトレンドを追加
         """
         logger.info("start: {:s}".format(inspect.currentframe().f_code.co_name))
+        if "trend60" in self.data.columns:
+            return
+
         self.data["trend60"] = self.data["trend"].shift(-1 * MINUTES_OF_HOURS)
+        self.data.to_csv("datasets/" + str(self.version) + ".csv", index=False)
 
     def add_columns_close_ratio(self):
         """
@@ -134,8 +136,12 @@ class BitcoinDataset:
         minute_list = [1, 2, 5, 10, 15, 30, 60, 120, 240, 480, 720, 1440, 2880, 10080]
         for i in minute_list:
             name = "close_ratio-" + str(i)
+            if name in self.data.columns:
+                continue
+
             self.data[name] = self.data["close"].shift(i)
             self.data[name] = self.data[name] / self.data["open"]
+            self.data.to_csv("datasets/" + str(self.version) + ".csv", index=False)
 
     def convert_hlc_to_ratio(self):
         """
@@ -144,4 +150,9 @@ class BitcoinDataset:
         logger.info("start: {:s}".format(inspect.currentframe().f_code.co_name))
         columns = ["high", "low", "close"]
         for column in columns:
-            self.data[column + "_ratio"] = self.data[column] / self.data["open"]
+            name = column + "_ratio"
+            if name in self.data.columns:
+                continue
+
+            self.data[name] = self.data[column] / self.data["open"]
+            self.data.to_csv("datasets/" + str(self.version) + ".csv", index=False)
