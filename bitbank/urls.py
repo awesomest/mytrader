06e96e247645ddbe373aeac51b7a23acd1b1ca94
Shuhei Katori <ihad.19.n@gmail.com>
2020-05-28 03:07:24 +0900

    Add fetch page

--- bitbank/urls.py
+++ bitbank/urls.py
@@ -6,4 +6,6 @@ from . import views
 app_name = "bitbank"  # pylint: disable=invalid-name
 urlpatterns = [
     path("", views.index, name="index"),
+    path("fetch/", views.fetch, name="fetch"),
+    path("results/<str:results_str>", views.results, name="results"),
 ]
--- bitbank/views.py
+++ bitbank/views.py
@@ -1,7 +1,19 @@
 """views.py"""
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 def index(request):
     """index"""
     return render(request, "bitbank/index.html")
+
+
+def fetch(_):
+    """fetch"""
+    return HttpResponseRedirect(reverse("bitbank:results", args=("success",)))
+
+
+def results(request, results_str):
+    """results"""
+    return render(request, "bitbank/results.html", {"results": results_str})
