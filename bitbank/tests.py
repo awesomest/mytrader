ed35c823b13c311b551c16f6d6480787e9b59d23
Shuhei Katori <ihad.19.n@gmail.com>
2020-05-29 06:20:42 +0900

    Add select_latest_date()

--- bitbank/tests.py
+++ bitbank/tests.py
@@ -1,3 +1,45 @@
+"""tests.py"""
+import datetime as dt
 from django.test import TestCase
+from . import views
+from .models import Candlestick
 
-# Create your tests here.
+
+def create_oldest_candlestick():
+    """create_oldest_candlestick"""
+    return Candlestick.objects.create(
+        unixtime=1487042760,
+        open=111332,
+        high=111332,
+        low=111332,
+        close=111332,
+        volume=0.0058,
+    )
+
+
+def create_latest_candlestick():
+    """create_last_candlestick"""
+    return Candlestick.objects.create(
+        unixtime=1497042760,
+        open=111332,
+        high=111332,
+        low=111332,
+        close=111332,
+        volume=0.0058,
+    )
+
+
+class CandlestickModelTests(TestCase):
+    """CandlestickModelTests"""
+
+    def test_select_oldest_date(self):
+        """test_select_oldest_date"""
+        latest_date = views.select_latest_date()
+        self.assertEqual(latest_date, dt.date(2017, 2, 14))
+
+    def test_select_last_date(self):
+        """test_select_last_date"""
+        create_oldest_candlestick()
+        create_latest_candlestick()
+        latest_date = views.select_latest_date()
+        self.assertEqual(latest_date, dt.date(2017, 6, 10))
--- bitbank/views.py
+++ bitbank/views.py
@@ -1,7 +1,17 @@
 """views.py"""
+import datetime as dt
+from logging import getLogger, basicConfig, DEBUG
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from bitbank.models import Candlestick
+
+FORMATTER = "%(levelname)8s : %(asctime)s : %(message)s"
+basicConfig(format=FORMATTER)
+logger = getLogger(__name__)
+logger.setLevel(DEBUG)
+
+# from .modules import candlestick
 
 
 def index(request):
@@ -17,3 +27,18 @@ def fetch(_):
 def results(request, results_str):
     """results"""
     return render(request, "bitbank/results.html", {"results": results_str})
+
+
+def select_latest_date():
+    """select_latest_date"""
+    try:
+        latest_unixtime = Candlestick.objects.order_by("-unixtime")[0].unixtime
+    except (Candlestick.DoesNotExist, IndexError):
+        oldest_date = dt.date(2017, 2, 14)  # TODO: 適当に決めた日付なので要改善
+        return oldest_date
+    else:
+        latest_datetime = dt.datetime.fromtimestamp(latest_unixtime)
+        latest_date = dt.date(
+            latest_datetime.year, latest_datetime.month, latest_datetime.day
+        )
+        return latest_date
