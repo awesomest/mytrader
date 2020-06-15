dc3e9919b34a2c1d7406995f50850445a4f1febd
Shuhei Katori <ihad.19.n@gmail.com>
2020-06-16 01:16:50 +0900

    Resolve warning of mysql strict mode

--- mytrader/settings/local.py
+++ mytrader/settings/local.py
@@ -7,6 +7,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+# Security
+# SECURE_HSTS_SECONDS = 31536000
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# SECURE_HSTS_PRELOAD = True
+# SECURE_SSL_REDIRECT = True
+# SECURE_REFERRER_POLICY = "same-origin"
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+
 # Running locally so connect to either a local MySQL instance or connect to
 # Cloud SQL via the proxy. To start the proxy via command line:
 #
@@ -17,10 +26,12 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "HOST": "127.0.0.1",
-        "PORT": "4306",
         "NAME": "test",
         "USER": "eraise",
         "PASSWORD": "eraise",
-        "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'",},
+        "PORT": "4306",
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES,NO_ZERO_DATE,NO_ZERO_IN_DATE,ERROR_FOR_DIVISION_BY_ZERO'",  # pylint: disable=line-too-long
+        },
     }
 }
